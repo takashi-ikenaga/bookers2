39812cd8a2f51f97394ae47ef4892a4ba9dbd6be
takashi-ikenaga <formail7300@gmail.com>
2021-08-28 11:52:04 +0000

    Initial Commit

--- db/migrate/20210825152959_devise_create_users.rb
+++ db/migrate/20210825152959_devise_create_users.rb
@@ -33,9 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
      t.string :name
-     t.text :introduction
-     t.string :profile_image_id
-      t.timestamps null: false
+    　t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
